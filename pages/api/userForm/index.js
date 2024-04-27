dfcf5456bbc53c362593d85efca19a1ea2532f6b
To <phucto91@gmail.com>
2024-04-27 14:42:01 -0500

    Added test page

--- pages/api/userForm/index.js
+++ pages/api/userForm/index.js
@@ -1,22 +1,22 @@
-// import { prisma } from "@/components/lib/prisma";
-// import { decryptPassword } from "@/components/lib/auth";
+import { prisma } from "@/components/lib/prisma";
+import { decryptPassword } from "@/components/lib/auth";
 
-// export default async function handler(req, res) {
-//   console.log("step 2");
-//   if (req.method === "GET") {
-//     try {
-//       const loot = await prisma.userForm.findFirst({
-//         where: {
-//           firstName: "Binbin",
-//         },
-//       });
-//       console.log(loot);
-//       console.log(decryptPassword(loot.password));
-//       return res.status(200).json(loot);
-//     } catch (error) {
-//       console.log(error.message);
-//     }
-//   } else {
-//     return res.status(500).json(error);
-//   }
-// }
+export default async function handler(req, res) {
+  // console.log("step 2");
+  // if (req.method === "GET") {
+  //   try {
+  //     const loot = await prisma.userForm.findFirst({
+  //       where: {
+  //         firstName: "Binbin",
+  //       },
+  //     });
+  //     console.log(loot);
+  //     console.log(decryptPassword(loot.password));
+  //     return res.status(200).json(loot);
+  //   } catch (error) {
+  //     console.log(error.message);
+  //   }
+  // } else {
+  //   return res.status(500).json(error);
+  // }
+}
